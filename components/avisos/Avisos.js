9f0942704dabf4ad39f12f12044e2002ecece8c9
Fernando Bartholomeu <fernandobrscunha@gmail.com>
2020-11-25 00:05:25 -0300

    Selecionar e alterar um aviso

--- components/avisos/Avisos.js
+++ components/avisos/Avisos.js
@@ -18,6 +18,23 @@ function salvar(aviso) {
 }//fim do salvar
 
 /**
+ * Alterar um aviso no banco de dados
+ * @param {object} aviso O aviso deve estar no seguinte formato: 
+ * {titulo: <string>, data: <string>, mensagem: <string>}
+ * @param {int} id ID do aviso
+ * @returns {object} Mensagem de sucesso ou de erro
+ */
+function editar(aviso, id){
+  return db('avisos').where('ID_avisos', id).update(aviso)
+  .then( _ => {
+    return { tipo: "sucesso", corpo: "Aviso alterado com sucesso!" }
+  })
+  .catch(erro => {
+    return { tipo: "erro", corpo: "Erro: " + erro }
+  })
+}//fim do editar
+
+/**
  * Seleciona todos os avisos cadastrados
  * @returns {object} Objeto com todos os avisos cadastrados ou
  * uma mensagem de erro
@@ -31,6 +48,19 @@ function selecionarTodos(){
 }//fim do selecionarTodos
 
 /**
+ * Seleciona um aviso
+ * @param {*} id ID do aviso que será selecionado
+ * @returns {object} Objeto com o aviso selecionado
+ */
+function selecionarAviso(id){
+  return db.select().from('avisos').where('ID_avisos',id).first()
+    .then(aviso => { return aviso })
+    .catch(erro => {
+      return { tipo: "erro", corpo: "Erro: " + erro }
+    })
+}//fim do selecionarAviso
+
+/**
  * Função que exclui um aviso do banco de dados
  * @param {int} id Id do aviso
  */
@@ -38,4 +68,10 @@ function excluir(id){
   return db.del().from('avisos').where('ID_avisos',id)
 }
 
-module.exports = {salvar, selecionarTodos, excluir}
+module.exports = {
+  salvar, 
+  selecionarTodos, 
+  selecionarAviso, 
+  excluir,
+  editar
+}
